6acc6dab745a42f99e76f43521938fa2aaa0392f
raheems <raheem@gmail.com>
2020-03-12 20:42:52 -0400

    Added/updated content

--- _deploy.sh
+++ _deploy.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-set -e
+git add --all
+git commit -m "Added/updated content"
+git push origin master
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-
-git config --global user.email "raheem@gmail.com"
-git config --global user.name "Enayetur Raheem"
-
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-cd book-output
-cp -r ../_book/* ./
-git add --all *
-git commit -m"Update the book" || true
-git push -q origin gh-pages
